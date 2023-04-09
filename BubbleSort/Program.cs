2dc203158a1f4a29e6c711ecc1668f805f03baf6
qkrgustn <sdh210212@sdh.hs.kr>
2023-04-10 08:20:25 +0900

    오름차순 / 내림차순 버블정렬

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace BubbleSort
+{
+    class BubbleSort
+    {
+        static void Main()
+        {
+            int[] arr = {4, 1, 2 ,3 ,5 ,7, 8, 10, 9, 6, 4, 4, 4 };
+
+            DescendingOrder(arr);
+            AscendingOrder(arr);
+        }
+
+        static void DescendingOrder(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+
+                    if (arr[j] < arr[j + 1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        static void AscendingOrder(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
